b40fafb12557e4d84fab32e5bb8332e787420553
xuannghi19 <xuannghi19t@outlook.com>
2025-08-11 08:32:05 +0700

    add env.product

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
+  user_id: '22161998',
+  baseurl: 'https://api.themoviedb.org/3',
+  bearer_key: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkM2UxZWI1ZGVlZGU2MDZjMzMzYTJkN2RlZThkN2YxMCIsIm5iZiI6MTc1MzA4MDQyMC42NzM5OTk4LCJzdWIiOiI2ODdkZTI2NGEzM2NiMzI3YTM3MmNhZWEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZrOBAR2EfWFMl-9-C4Qb2_d0YRJWeMO4LNHusZZ3BPM',
   production: true
 };
